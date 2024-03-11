3bbb1ad37e737d85b26fef5d4bf685e03ab75a63
Aswin-Shaji <aswin2000.ak@gmail.com>
2024-03-11 11:06:59 +0530

    Add track request options to orders model

--- src/models/orders.ts
+++ src/models/orders.ts
@@ -54,3 +54,17 @@ export const insertTypeOptions: Array<KeyValueSelects> = [
   { key: '12', displayName: 'STRAIGHT STOCKBRIDGE' },
   { key: '13', displayName: 'WATERTON' }
 ];
+
+export const trackRadii: Array<number> = [ 12, 15 ];
+
+export const trackTypeOptions: Array<KeyValueSelects> = [
+  { key: 'standard-lift-bracket', displayName: 'STANDARD LIFT BRACKET MOUNT' },
+  { key: 'standard-lift-angle', displayName: 'STANDARD LIFT ANGLE MOUNT' },
+  { key: 'standard-lift-long-angle', displayName: 'STANDARD LIFT LONG ANGLE' },
+  { key: 'vertical-lift-bracket', displayName: 'VERTICAL LIFT BRACKET MOUNT' },
+  { key: 'vertical-lift-angle', displayName: 'VERTICAL LIFT ANGLE MOUNT' }
+];
+
+export const specialTrackRequestOptions: Array<KeyValueSelects> = [
+  { key: 'high-lift-kits', displayName: 'HIGH LIFT KITS' }, { key: 'low-head-room-kits', displayName: 'LOW HEAD ROOM KITS' }
+];
