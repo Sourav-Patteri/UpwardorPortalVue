1438e3fcedd78c8a45126d19076fe88a2e8b54ed
Aswin-Shaji <aswin2000.ak@gmail.com>
2024-03-11 09:13:35 +0530

    Add stamp patterns to orders model

--- src/models/orders.ts
+++ src/models/orders.ts
@@ -3,6 +3,11 @@ export interface MeasurementOptions {
   inches?: Array<string>
 }
 
+export interface StampPattern {
+  key: string,
+  displayName: string
+}
+
 export const doorWidths: Array<string | MeasurementOptions> = [
   { foot: '08' }, { foot: '09' }, { foot: '10' }, { foot: '12' },
   { foot: '14' }, { foot: '16' }, { foot: '18' }
@@ -37,4 +42,13 @@ export const doorHeights: Array<MeasurementOptions> = [
     foot: '12',
     inches: [ '0' ]
   }
+];
+
+export const stampPatterns: Array<StampPattern> = [
+  { key: 'FLUSH', displayName: 'Flush' },
+  { key: 'TRAFALGAR', displayName: 'Trafalgar' },
+  { key: 'BC', displayName: 'Bronte Creek' },
+  { key: 'BCXL', displayName: 'Bronte Creek XL' },
+  { key: 'SH', displayName: 'Sheridan' },
+  { key: 'SHXL', displayName: 'Sheridan XL' }
 ];
