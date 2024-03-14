b44c022b7314f2eaacb1f23ba972b056e7294901
Aswin-Shaji <aswin2000.ak@gmail.com>
2024-03-15 00:56:29 +0530

    Add MainContent component to render headers and views

---
+++ src/components/MainContent.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <div class='header'>
+      <img src='@/assets/UpwardorLogo.png' alt='Upwardor Logo' width='140' />
+
+      <h2 class='page-title'>{{ title }}</h2>
+    </div>
+
+    <main>
+      <slot></slot>
+    </main>
+  </div>
+</template>
+
+<script setup lang='ts'>
+export interface Props {
+  title: string
+}
+
+const props = defineProps<Props>();
+</script>
+
+<style scoped lang='scss'>
+.header {
+  display: flex;
+  align-items: flex-end;
+  gap: 40px;
+  margin: 40px 32px;
+
+  .page-title {
+    font-size: 30px;
+    font-weight: 400;
+    padding: 0;
+    margin: 0;
+    margin-bottom: 12px;
+  }
+}
+</style>
