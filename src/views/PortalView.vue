7eb46480c8d95137664592f8923c027cbb4d0c0e
Aswin-Shaji <aswin2000.ak@gmail.com>
2024-03-15 00:49:42 +0530

    Add PortalView component

---
+++ src/views/PortalView.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class='main'>
+    <h2>Upwardor Portal</h2>
+
+    <quote-generator></quote-generator>
+  </div>
+</template>
+
+<script setup lang='ts'>
+import QuoteGenerator from '@/components/QuoteGenerator.vue';
+</script>
+
+<style scoped lang='scss'>
+.main {
+}
+</style>
