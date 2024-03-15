b54e4c5a22589992e51f4da903461a751c3cd619
Aswin-Shaji <aswin2000.ak@gmail.com>
2024-03-15 10:27:07 +0530

    Remove page header from PortalView component

--- src/views/PortalView.vue
+++ src/views/PortalView.vue
@@ -1,16 +1,7 @@
 <template>
-  <div class='main'>
-    <h2>Upwardor Portal</h2>
-
-    <quote-generator></quote-generator>
-  </div>
+  <quote-generator></quote-generator>
 </template>
 
 <script setup lang='ts'>
 import QuoteGenerator from '@/components/QuoteGenerator.vue';
 </script>
-
-<style scoped lang='scss'>
-.main {
-}
-</style>
